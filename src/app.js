90552e6c2bb9b3a692e4bd3ba79c6d53067e5a54
nishantrajput1 <nishant.rajput0502@gmail.com>
2026-04-03 23:03:06 +0530

    Learned to connect to backend and then make all the 4 api calls easily

--- src/app.js
+++ src/app.js
@@ -1,42 +1,40 @@
 const express = require('express');
+const noteModel = require("./models/note.model");
 const app = express();
 app.use(express.json());
-const notes = [
-
-]
-// title and description dena hoga user ko
-// front end se data server par aayega
-// POST method ka istemaal karenge 
-app.post('/notes', (req, res) => {
-    // AGAR frontedn se koi bhi data
-    // aa raha hai to use access karne ke liye
-    // req ka use karna hota haia 
-    console.log(req.body);
-    notes.push(req.body);
+app.post("/notes",async (req,res)=>{
+    const data = req.body /* {title,description}*/
+    await noteModel.create({
+        title : data.title,
+        description : data.description
+    })
     res.status(201).json({
-        message: "note created successfully"
+        message : "Note created"
     })
 })
-app.get('/notes', (req, res) => {
+
+app.get("/notes",async (req,res)=>{
+    const notes = await noteModel.find();
     res.status(200).json({
-        message: "notes fetched successfully",
-        notes: notes
+        message : "Notes fetched successfully",
+        notes : notes
     })
 })
-app.delete('/notes/:index',(req,res)=>{
-    const index = req.params.index;
-    delete notes[index];
+app.delete("/notes/:id",async(req,res)=>{
+    const id = req.params.id;
+    await noteModel.findOneAndDelete({
+        _id : id
+    })
     res.status(200).json({
-        message : "note deleted successfully"
+        message:  "Note deleted successfully"
     })
 })
-app.patch('/notes/:index',(req,res)=>{
-    const index = req.params.index; 
+app.patch("/notes/:id", async(req,res)=>{
+    const id = req.params.id;
     const description = req.body.description;
-    notes[index].description = description;
+    await noteModel.findOneAndUpdate({_id : id},{description : description})
     res.status(200).json({
-        message:"note updated successfully"
+        message : "Note updated successfully"
     })
 })
-// idhar main kaam hai server ko create karna
 module.exports = app;
